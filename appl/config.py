85d6bab7ec16b05db3fb68c89f8f2c39fbd18e7b
N-Ghi <n.iyankijij@alustudent.com>
2024-06-22 10:10:41 +0200

    some of all

--- appl/config.py
+++ appl/config.py
@@ -1,5 +1,3 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
-
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/Smart_Waste'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- appl/db_models.py
+++ appl/db_models.py
@@ -4,8 +4,7 @@ from datetime import datetime
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), primary_key=True, nullable=False)
     role = db.Column(db.String(20), nullable=False)  # 'household', 'service', 'admin'
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
@@ -20,45 +19,33 @@ class User(db.Model):
     }
 
     def __repr__(self):
-        return f'<User id={self.id} username={self.username} role={self.role}>'
+        return f'<username={self.username} role={self.role}>'
 
-class Credentials(User):
-    __tablename__ = 'credentials'
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, autoincrement=True)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'credentials',
-        'inherit_condition': (id == User.id)
-    }
-
-    def __repr__(self):
-        return f'<Credentials id={self.id} email={self.email}>'
 
 class WasteCollection(db.Model):
     __tablename__ = 'wastecollection'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    username = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     status = db.Column(db.String(20), nullable=False)  # 'scheduled', 'in_progress', 'completed', 'cancelled', 'missed', 'delayed'
     waste_type = db.Column(db.Integer, db.ForeignKey('wastetype.id'), nullable=False)
     location = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
-        return f'<WasteCollection id={self.id} user_id={self.user_id} status={self.status}>'
+        return f'<WasteCollection id={self.id} username={self.username} status={self.status}>'
 
 class RecyclingEffort(db.Model):
     __tablename__ = 'recyclingeffort'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    username = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     waste_name = db.Column(db.String(55), db.ForeignKey('wastetype.name'), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f'<RecyclingEffort id={self.id} user_id={self.user_id} date={self.date} waste_type_id={self.waste_type_id}>'
+        return f'<RecyclingEffort id={self.id} username={self.username} date={self.date} waste_name={self.waste_name}>'
 
 class WasteType(db.Model):
     __tablename__ = 'wastetype'
@@ -73,7 +60,7 @@ class Locations(db.Model):
     __tablename__ = 'locations'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(55), nullable=False)
+    name = db.Column(db.String(55), unique=True, nullable=False)
 
     def __repr__(self):
         return f'<Location id={self.id} name={self.name}>'
@@ -82,7 +69,7 @@ class WasteCollectionSchedule(db.Model):
     __tablename__ = 'waste_collection_schedule'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    username = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
     collection_date = db.Column(db.DateTime, nullable=False)
     waste_type = db.Column(db.String(50), nullable=False)
     status = db.Column(db.String(50), nullable=False, default='scheduled')  # 'scheduled', 'completed', etc.
@@ -91,13 +78,13 @@ class WasteCollectionSchedule(db.Model):
     user_nrel = db.relationship('User', backref='waste_collection_schedules', lazy=True)
 
     def __repr__(self):
-        return f'<WasteCollectionSchedule id={self.id} user_id={self.user_id} collection_date={self.collection_date} status={self.status}>'
+        return f'<WasteCollectionSchedule id={self.id} username={self.username} collection_date={self.collection_date} status={self.status}>'
 
 class Notification(db.Model):
     __tablename__ = 'notification'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    username = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
     message = db.Column(db.String(200), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     type = db.Column(db.String(50), nullable=False)  # 'reminder', 'confirmation', 'update', 'deletion'
@@ -105,4 +92,5 @@ class Notification(db.Model):
     user_nrel = db.relationship('User', backref='notifications_rel', lazy=True)
 
     def __repr__(self):
-        return f'<Notification id={self.id} user_id={self.user_id} message={self.message} type={self.type}>'
+        return f'<Notification id={self.id} username={self.username} message={self.message} type={self.type}>'
+    
